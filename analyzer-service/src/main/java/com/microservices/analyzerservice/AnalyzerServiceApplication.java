b7db6aef7bd02c3f98a613dff18cc2f255d45e2b
Dan Serbyn <dev.serbyn@gmail.com>
2022-04-30 14:37:33 +0300

    Analyzer-Service: Initial Commit

---
+++ analyzer-service/src/main/java/com/microservices/analyzerservice/AnalyzerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.analyzerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerServiceApplication.class, args);
+	}
+
+}
---
+++ analyzer-service/src/test/java/com/microservices/analyzerservice/AnalyzerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.analyzerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
